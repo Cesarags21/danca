ee4e7f56ae071b80c52e33f20e7924c2da717ddf
Gabriel Botelho <gabrielrbotelho@outlook.com>
2021-10-28 16:36:28 -0300

    sasas

--- lib/pages/danca/centro Oeste/boi.dart
+++ lib/pages/danca/centro Oeste/boi.dart
@@ -2,9 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:regiao2/core/app_colors.dart';
 import 'package:regiao2/pages/home/centroOeste.dart';
 
-
 class Boi extends StatefulWidget {
-  const Boi({ Key? key }) : super(key: key);
+  const Boi({Key? key}) : super(key: key);
 
   @override
   _BoiState createState() => _BoiState();
@@ -12,7 +11,6 @@ class Boi extends StatefulWidget {
 
 class _BoiState extends State<Boi> {
   @override
-  
   bool _expanded = false;
   bool _expanded1 = false;
   bool _expanded2 = false;
@@ -21,35 +19,40 @@ class _BoiState extends State<Boi> {
   var _test = "Full Screen";
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar( 
-        title: Center(child: Text("Danças Típicas")),
-        backgroundColor: AppColors.primaria01
-        
+      appBar: AppBar(
+        title: Text('Danças Típicas'),
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              /*begin: Alignment.topRight,
+              end: Alignment.bottomLeft,*/
+              stops: [
+                0.4,
+                0.8,
+              ],
+              colors: <Color>[Color(0xFF008000), Color(0xFFFFD700)],
+            ),
+          ),
+        ),
       ),
-
-      
-      body:ListView(
+      body: ListView(
         children: [
-
           _dancaFaixa(CentroOeste()),
-
           Divider(
             height: 10,
             thickness: 0.2,
             color: Colors.white,
           ),
-
-           Padding(
-            padding: const EdgeInsets.only(
-              top: 30, right: 10, left: 30, bottom: 15
-            ),
+          Padding(
+            padding:
+                const EdgeInsets.only(top: 30, right: 10, left: 30, bottom: 15),
             child: Text('Boi à Serra:',
                 style: TextStyle(
-                  fontSize: 30, 
+                  fontSize: 30,
                 )),
           ),
 
-        _buildConteudo("Conheça a dança", 
+          /*_buildConteudo("Conheça a dança", 
         "ashufhrghergioerhitgerg" + 
         "pjfoprjorpjogktgotkhotkh´hkrhr" + 
         "opfjojfropgjkrokrg",
@@ -62,116 +65,279 @@ class _BoiState extends State<Boi> {
         "pjfoprjorpjogktgotkhotkh´hkrhr" + 
         "opfjojfropgjkrokrg",
         true
-        ),
-            
-      
-     ],
+        ),*/
+
+          Column(children: [
+            Container(
+              margin: EdgeInsets.all(10),
+              color: Colors.green,
+              child: ExpansionPanelList(
+                animationDuration: Duration(milliseconds: 2000),
+                children: [
+                  ExpansionPanel(
+                    headerBuilder: (context, isExpanded) {
+                      return ListTile(
+                        title: Text(
+                          'Conheça a dança',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      );
+                    },
+                    body: ListTile(
+                      title: Text(
+                          'https://www.youtube.com/watch?v=OvDt_SZN4y8\n' +
+                              'https://www.youtube.com/watch?v=mcvfH9HHhr0',
+                          style: TextStyle(color: Colors.black)),
+                    ),
+                    isExpanded: _expanded,
+                    canTapOnHeader: true,
+                  ),
+                ],
+                dividerColor: Colors.grey,
+                expansionCallback: (panelIndex, isExpanded) {
+                  _expanded = !_expanded;
+                  setState(() {});
+                },
+              ),
+            ),
+          ]),
+          Column(children: [
+            Container(
+              margin: EdgeInsets.all(10),
+              color: Colors.green,
+              child: ExpansionPanelList(
+                animationDuration: Duration(milliseconds: 2000),
+                children: [
+                  ExpansionPanel(
+                    headerBuilder: (context, isExpanded) {
+                      return ListTile(
+                        title: Text(
+                          'História',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      );
+                    },
+                    body: ListTile(
+                      title: Text(
+                          'Houve tempo em que o Boi-à-Serra foi muito difundido em Mato Grosso, principalmente nas localidades de Santo Antônio do Leverger, Varginha, Carrapicho, Engenho Velho, Bom Sucesso e Maravilha, onde existiam grandes canaviais e a atividade econômica predominante eram os engenhos de açúcar. A dança do Boi-à-Serra hoje, consegue ainda manter suas características iniciais apenas na localidade de Varginha, no município de Santo Antônio do Leverger. Lá as pessoas ainda cantam uma toada que conta toda a trajetória de vida e morte de um boi que é capturado por destemidos vaqueiros, enquanto dançam. O Boi-à-Serra é um folguedo do carnaval mato-grossense, porém, segundo alguns tiradores, o Boi-à-Serra pode ser dançado em qualquer festa.',
+                          style: TextStyle(color: Colors.black)),
+                    ),
+                    isExpanded: _expanded1,
+                    canTapOnHeader: true,
+                  ),
+                ],
+                dividerColor: Colors.grey,
+                expansionCallback: (panelIndex, isExpanded) {
+                  _expanded1 = !_expanded1;
+                  setState(() {});
+                },
+              ),
+            ),
+          ]),
+          Column(children: [
+            Container(
+              margin: EdgeInsets.all(10),
+              color: Colors.green,
+              child: ExpansionPanelList(
+                animationDuration: Duration(milliseconds: 2000),
+                children: [
+                  ExpansionPanel(
+                    headerBuilder: (context, isExpanded) {
+                      return ListTile(
+                        title: Text(
+                          'Vestimentas',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      );
+                    },
+                    body: ListTile(
+                      title: Text(
+                          'O boi, principal personagem, é feito da seguinte forma: primeiro monta-se uma estrutura de madeira leve e flexível conhecida como "melado de pomba"; depois, cobrem essa estrutura com um cobertor tipo "seca-poço", formando o corpo do boi, de cor semelhante à cor do animal; a cabeça do boi da brincadeira é a própria caveira do animal, seca, que é pintada com uma tinta escura, recebendo ainda algo para representar os olhos do boi, que pode ser dois botões, um de cada lado. Atualmente, algumas pessoas confeccionam o Boi-à-serra assemelhando-o ao Bumba-meu-boi de estados nordestinos do Brasil, cobrindo a mesma estrutura de madeira e a caveira do boi com tecido estampado. Uma pessoa vai dentro do boi, dançando ao carregá-lo, dando vida ao brinquedo, que avança em direção à platéia, como se fosse "chifrar" os presentes. Outros personagens, não menos interessantes, são: a cabeça de apá, a mãe do morro, o tuiuiú, a ema, o morcego, o cavalo sem cabeça; esses, são variáveis, de um a outro grupo. ',
+                          style: TextStyle(color: Colors.black)),
+                    ),
+                    isExpanded: _expanded2,
+                    canTapOnHeader: true,
+                  ),
+                ],
+                dividerColor: Colors.grey,
+                expansionCallback: (panelIndex, isExpanded) {
+                  _expanded2 = !_expanded2;
+                  setState(() {});
+                },
+              ),
+            ),
+          ]),
+          Column(children: [
+            Container(
+              margin: EdgeInsets.all(10),
+              color: Colors.green,
+              child: ExpansionPanelList(
+                animationDuration: Duration(milliseconds: 2000),
+                children: [
+                  ExpansionPanel(
+                    headerBuilder: (context, isExpanded) {
+                      return ListTile(
+                        title: Text(
+                          'Músicas',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      );
+                    },
+                    body: ListTile(
+                      title: Text('https://www.youtube.com/watch?v=07UTOrhONH0',
+                          style: TextStyle(color: Colors.black)),
+                    ),
+                    isExpanded: _expanded3,
+                    canTapOnHeader: true,
+                  ),
+                ],
+                dividerColor: Colors.grey,
+                expansionCallback: (panelIndex, isExpanded) {
+                  _expanded3 = !_expanded3;
+                  setState(() {});
+                },
+              ),
+            ),
+          ]),
+          Column(children: [
+            Container(
+              margin: EdgeInsets.all(10),
+              color: Colors.green,
+              child: ExpansionPanelList(
+                animationDuration: Duration(milliseconds: 2000),
+                children: [
+                  ExpansionPanel(
+                    headerBuilder: (context, isExpanded) {
+                      return ListTile(
+                        title: Text(
+                          'Aprenda a dançar',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      );
+                    },
+                    body: ListTile(
+                      title: Text('AAAAAAAAAAAAAAAAAAAA',
+                          style: TextStyle(color: Colors.black)),
+                    ),
+                    isExpanded: _expanded4,
+                    canTapOnHeader: true,
+                  ),
+                ],
+                dividerColor: Colors.grey,
+                expansionCallback: (panelIndex, isExpanded) {
+                  _expanded4 = !_expanded4;
+                  setState(() {});
+                },
+              ),
+            ),
+          ]),
+          Column(children: [
+            new Image.asset(
+              'images/boi.jpg',
+              width: 370.0,
+              height: 180.0,
+              fit: BoxFit.cover,
+            )
+          ]),
+        ],
       ),
-    
-  );
+    );
   }
 
-  Widget _dancaFaixa(rota){
-      return Container(
-        height: 50,
-        width: MediaQuery.of(context).size.width,
-        decoration: BoxDecoration(color: Colors.blue[900], boxShadow: [
-          BoxShadow(
-            color: Colors.blue,
-          ),
-        ]),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Row(
-              children: [
-                IconButton(
-                  onPressed: () {
-                    Navigator.of(context).pushReplacement(MaterialPageRoute(
-                        builder: (context) => rota));
-                  },
-                  icon: Icon(Icons.arrow_back),
-                  iconSize: 30,
-                  color: Colors.white,
-                ),
-                Text(
-                  "Dança",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold),
-                ),
-              ],
-            ),
-          ],
+  Widget _dancaFaixa(rota) {
+    return Container(
+      height: 50,
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(color: Colors.blue[900], boxShadow: [
+        BoxShadow(
+          color: Colors.blue,
         ),
-      );
-    }
+      ]),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              IconButton(
+                onPressed: () {
+                  Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(builder: (context) => rota));
+                },
+                icon: Icon(Icons.arrow_back),
+                iconSize: 30,
+                color: Colors.white,
+              ),
+              Text(
+                "Dança",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
 
-  Widget _buildDanca(estado){
+  Widget _buildDanca(estado) {
     return Container(
       child: Center(
         child: Container(
-              color: AppColors.primaria03,
-              width: MediaQuery.of(context).size.width,
-              height: 63,
-              padding: EdgeInsets.all(15),
-              child: Column(
-                children: [
-                  Padding(padding: EdgeInsets.all(5)),
-                  Text(estado,
-                  style: TextStyle(
-                    color: AppColors.primaria02,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
+          color: AppColors.primaria03,
+          width: MediaQuery.of(context).size.width,
+          height: 63,
+          padding: EdgeInsets.all(15),
+          child: Column(
+            children: [
+              Padding(padding: EdgeInsets.all(5)),
+              Text(
+                estado,
+                style: TextStyle(
+                  color: AppColors.primaria02,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
                 ),
-                
-              ],
               ),
-                  
-            ),
+            ],
+          ),
+        ),
       ),
     );
   }
 
-  Widget _buildConteudo(titulo, texto, ex){
-    return Column(
-      children: [
-        Container(
+  Widget _buildConteudo(titulo, texto, ex) {
+    return Column(children: [
+      Container(
         margin: EdgeInsets.all(10),
         color: Colors.green,
         child: ExpansionPanelList(
-        animationDuration: Duration(milliseconds: 2000),
-        children: [
-          ExpansionPanel(
-            headerBuilder: (context, isEx) {
-              return ListTile(
-                
-                title: Text(titulo, style: TextStyle(color: Colors.black),),
-              );
-              
-            },
-            body:ListTile(
-              title: Text(texto,style: TextStyle(color: Colors.black)),
+          animationDuration: Duration(milliseconds: 2000),
+          children: [
+            ExpansionPanel(
+              headerBuilder: (context, isEx) {
+                return ListTile(
+                  title: Text(
+                    titulo,
+                    style: TextStyle(color: Colors.black),
+                  ),
+                );
+              },
+              body: ListTile(
+                title: Text(texto, style: TextStyle(color: Colors.black)),
+              ),
+              isExpanded: ex,
+              canTapOnHeader: true,
             ),
-          isExpanded: ex,
-          canTapOnHeader: true,
-          ),
-        ],
-        dividerColor: Colors.grey,
-        expansionCallback: (panelIndex, isExpanded) {
-        _expanded = !ex;
-          setState(() {
-
-          });
-        },
-
+          ],
+          dividerColor: Colors.grey,
+          expansionCallback: (panelIndex, isExpanded) {
+            _expanded = !ex;
+            setState(() {});
+          },
         ),
       ),
-      ]
-    );
+    ]);
   }
-
 }
-
--- lib/pages/inicio/index.dart
+++ lib/pages/inicio/index.dart
@@ -7,7 +7,7 @@ import 'package:regiao2/pages/home/norte.dart';
 import 'package:regiao2/pages/home/sudeste.dart';
 
 class Inicio extends StatefulWidget {
-  const Inicio({ Key? key }) : super(key: key);
+  const Inicio({Key? key}) : super(key: key);
 
   @override
   _InicioState createState() => _InicioState();
@@ -17,104 +17,141 @@ class _InicioState extends State<Inicio> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar( 
-        title: Center(child: Text("Danças Típicas")),
-        backgroundColor: AppColors.primaria01,
-        
+      appBar: AppBar(
+        title: Text('Danças Típicas'),
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              /*begin: Alignment.topRight,
+              end: Alignment.bottomLeft,*/
+              stops: [
+                0.4,
+                0.8,
+              ],
+              colors: <Color>[Color(0xFF008000), Color(0xFFFFD700)],
+            ),
+          ),
+        ),
       ),
       body: ListView(
         children: [
           //cabeçalho Danças Típicas e Azul
           Container(
-
-                  height: 50,
-                  width: MediaQuery.of(context).size.width,
-                  decoration: BoxDecoration(
-                    color: Colors.blue[900],
-                    boxShadow: [
-                      BoxShadow(
-                      color: Colors.blue,
-                    ),
-                    ]
-                  ),
-
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            height: 50,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(color: Colors.blue[900], boxShadow: [
+              BoxShadow(
+                color: Colors.blue,
+              ),
+            ]),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
                   children: [
-                    Row(
-                      children: [
-                        Padding(padding: EdgeInsets.all(8)),
-                        Text("Início", 
-                        style: TextStyle(
+                    Padding(padding: EdgeInsets.all(8)),
+                    Text(
+                      "Início",
+                      style: TextStyle(
                           color: Colors.white,
                           fontSize: 20,
-                          fontWeight: FontWeight.bold
-                        ),
-                      ),
-                      ],
+                          fontWeight: FontWeight.bold),
                     ),
-                    
                   ],
                 ),
-
+              ],
+            ),
           ),
-        
+
           Divider(
-                height: 10,
-                thickness: 0.2,
-                color: Colors.white,
-              ),
+            height: 10,
+            thickness: 0.2,
+            color: Colors.white,
+          ),
 
           //Texto
           Center(
             child: Container(
               padding: EdgeInsets.all(5),
-              child: Text("Escolha uma região e comece a dançar!",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
+              child: Text(
+                "Escolha uma região e comece a dançar!",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
           ),
 
           //mapa
-           Divider(
-                height: 10,
-                thickness: 0.2,
-                color: Colors.white,
+          Divider(
+            height: 10,
+            thickness: 0.2,
+            color: Colors.white,
           ),
 
-          _regBotao("CENTRO-OESTE", CentroOeste()),
-          _regBotao("NORDESTE", Nordeste()),
-          _regBotao("NORTE", NortePage()),
-          _regBotao("SUDESTE", Sudeste()),
-          _regBotao("SUL", HomePage()),
+          _regBotao("Centro-Oeste", CentroOeste()),
 
+          _regBotao("    Nordeste   ", Nordeste()),
+
+          _regBotao("       Norte       ", NortePage()),
+
+          _regBotao("    Sudeste     ", Sudeste()),
+
+          _regBotao("         Sul         ", HomePage()),
         ],
       ),
     );
   }
 
-  Widget _regBotao(regiao, rota){
-    return TextButton(
-      onPressed: (){
-        Navigator.of(context).pushReplacement(
-          MaterialPageRoute(
-            builder: (context) => rota
-          )
-        );
-      }, 
-      child: Text(
-        regiao,
-        style: TextStyle(
-          color: Colors.black,
-          fontSize: 25,
-        ),
-      ), 
-    
+  Widget _regBotao(regiao, rota) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          SizedBox(
+            height: 20,
+            width: 200,
+          ),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: Stack(
+              children: <Widget>[
+                Positioned.fill(
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: <Color>[
+                          Color(0xFF006400),
+                          Color(0xFF008000),
+                          Color(0xFF32CD32),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    padding: const EdgeInsets.all(16.0),
+                    primary: Colors.white,
+                    //fontweight muda a grosura da letra
+
+                    textStyle: const TextStyle(
+                        fontSize: 20, fontFamily: 'Montserrat-Bold'),
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(builder: (context) => rota),
+                    );
+                  },
+                  child: Text(regiao),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
-
 }

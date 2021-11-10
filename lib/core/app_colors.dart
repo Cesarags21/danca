295f2ba389f8203a738c9d74bd7a7cfbe445adb8
Cesar Augusto <augusto.2003cesar@gmail.com>
2021-11-10 08:34:15 -0300

    cores regioes

--- lib/core/app_colors.dart
+++ lib/core/app_colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppColors{
-  static final Color primaria01 = Colors.green.shade700;
+  static final Color primaria01 = Colors.green.shade900;
   static final Color primaria02 = Colors.blue.shade900;
   static final Color primaria03 = Colors.yellow.shade400;
   static final Color corFonte01 = Colors.white;
